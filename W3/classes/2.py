e06dc2ccc6ec1646e0a6b481b5ae03bfb9f0d311
Rakymzhan Zhabagin <98641240+qonstant@users.noreply.github.com>
2022-02-18 21:17:28 +0600

    Update 2.py

--- W3/classes/2.py
+++ W3/classes/2.py
@@ -5,7 +5,7 @@ class Shape():
         print(0)
 class Square(Shape):
     def __init__(self, length = 0):
-        Shape.__init__(self)
+        super(Square, self).__init__()
         self.length = length
     def print_area(self):
         self.area = int(self.length) ** 2
@@ -16,4 +16,5 @@ print('Area of the square is: ', end ='')
 # Area of the square
 Sqr.print_area()
 # The default area of the shape
+print('Default area of the shape is: ', end ='')
 Shp.print_area() 
