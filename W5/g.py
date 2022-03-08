1b3eea8702dab369221fd80c9f6b68e5a1a978c1
Rakymzhan Zhabagin <98641240+qonstant@users.noreply.github.com>
2022-03-08 18:01:56 +0600

    Update g.py

--- W5/g.py
+++ W5/g.py
@@ -2,6 +2,7 @@ import re
 def jylan_to_tuie(text):
     arr = text.split('_')
     arr = [x.capitalize() for x in arr]
+    arr[0] = arr[0].lower()
     text = ''.join(arr)
     return text
 print(jylan_to_tuie('snake_to_camel'))
